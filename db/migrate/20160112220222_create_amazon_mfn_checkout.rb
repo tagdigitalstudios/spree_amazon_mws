2d8e6533d1e9e13593f32411bf1a5f4b7d3a5e81
Tim Glen <tim@tag.ca>
2016-01-14 23:29:53 -0500

    renamed table

--- db/migrate/20160112220222_create_amazon_mfn_checkout.rb
+++ db/migrate/20160112220222_create_amazon_mfn_checkout.rb
@@ -1,6 +1,6 @@
 class CreateAmazonMfnCheckout < ActiveRecord::Migration
   def change
-    create_table :amazon_mfn_checkouts do |t|
+    create_table :spree_amazon_mfn_checkouts do |t|
       t.string     :amazon_order_id
       t.references :order
       t.references :payment_method
