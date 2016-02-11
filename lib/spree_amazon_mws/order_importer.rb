76d44a70ce6de7abbd1ae8f2864415a1d79ea9dc
Tim Glen <tim@tag.ca>
2016-02-11 11:54:36 -0500

    only fetch orders if we don't already have some loaded

--- lib/spree_amazon_mws/order_importer.rb
+++ lib/spree_amazon_mws/order_importer.rb
@@ -3,7 +3,7 @@ module SpreeAmazonMws
     attr_accessor :amazon_orders
 
     def import_recent_orders(since=1.day.ago)
-      @amazon_orders = order_fetcher.get_orders(import_recent_orders_options(since))
+      @amazon_orders ||= order_fetcher.get_orders(import_recent_orders_options(since))
       # put this into a transaction to make it atomic
       Spree::Order.transaction do
         @amazon_orders.map do |amazon_order|
