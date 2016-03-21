8dbb349732a45cfc6ccb8612f85bddaf5d432e9c
Tim Glen <tim@tagstudios.io>
2016-03-21 16:49:53 -0400

    Specify the shipment mailer in Spree

--- app/models/spree/shipment_handler_decorator.rb
+++ app/models/spree/shipment_handler_decorator.rb
@@ -1,6 +1,6 @@
 Spree::ShipmentHandler.class_eval do
   private
     def send_shipped_email
-      ShipmentMailer.shipped_email(@shipment.id).deliver if !@shipment.order.amazon_order?
+      Spree::ShipmentMailer.shipped_email(@shipment.id).deliver if !@shipment.order.amazon_order?
     end
 end
